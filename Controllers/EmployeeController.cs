a603e5a0e9cf2c1275bc7d86863445a0b819f221
Chinthaka Jayasooriya <jayasooriya.sc@gmail.com>
2024-06-30 10:21:24 +0530

    first commit for my first app using c#

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
